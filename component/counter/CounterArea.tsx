7895634185d34e1794b825f28e0644f954705411
Ronit Pathak <ronitpathak12345@gmail.com>
2025-04-23 22:28:35 +0530

    coaching12

--- component/counter/CounterArea.tsx
+++ component/counter/CounterArea.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import CountUp from "react-countup";
+
 type Props = {
   marginTop: string;
   showAll: boolean;
 };
+
 const CounterArea = ({ marginTop, showAll }: Props) => {
   const [isVisible, setIsVisible] = useState(false);
   const onVisibilityChange = (entries: IntersectionObserverEntry[]) => {
@@ -35,32 +37,33 @@ const CounterArea = ({ marginTop, showAll }: Props) => {
       }
     };
   }, []);
+
   return (
     <div className={`tf__counter_area ${marginTop}`}>
       <div className="tf__counter2_overlay">
-        <ul className=" d-flex flex-wrap">
+        <ul className="d-flex flex-wrap">
           <li className="tf__single_counter">
             <h2 className="counter">
-              {isVisible && <CountUp start={1} end={22} duration={5} />}
+              {isVisible && <CountUp start={1} end={22} />}
             </h2>
-            <h4>Successflly Trained</h4>
+            <h4>Successfully Trained</h4>
           </li>
           <li className="tf__single_counter">
             <h2 className="counter">
-              {isVisible && <CountUp start={1} end={69} duration={5} />}
+              {isVisible && <CountUp start={1} end={69} />}
             </h2>
             <h4>Keywords Updated</h4>
           </li>
           <li className="tf__single_counter">
             <h2 className="counter">
-              {isVisible && <CountUp start={1} end={56} duration={5} />}
+              {isVisible && <CountUp start={1} end={56} />}
             </h2>
             <h4>Google Search</h4>
           </li>
           {showAll && (
             <li className="tf__single_counter">
               <h2 className="counter">
-                {isVisible && <CountUp start={1} end={13} duration={5} />}
+                {isVisible && <CountUp start={1} end={13} />}
               </h2>
               <h4>Success Rate</h4>
             </li>
