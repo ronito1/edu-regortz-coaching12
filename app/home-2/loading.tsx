0c5d0b6163dd3727e90764afbc735d276db83c82
Ronit Pathak <ronitpathak12345@gmail.com>
2025-04-23 21:49:26 +0530

    coaching12

---
+++ app/home-2/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Loading = () => {
+  return (
+    <div className="home-2-loading loading-container">
+      <div className="Edu Regortz-logo">Edu Regortz</div>
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
